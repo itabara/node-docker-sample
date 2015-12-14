ac704b05f54b69c6b87077085bb1f333f03270f4
Iulian <itabara@live.com>
2015-12-14 23:24:05 +0200

    Another change to the node service

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ var os = require("os");
 var app = express();
 var hostname = os.hostname();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
-  res.send('<html><body>Hello from Node.js container: ' + hostname + '</body></html>');
+  res.send('<html><body>Hello from Node.js container: ' + hostname + '<br>Port is: ' + port + '</body></html>');
 });
 
-var port = process.env.PORT || 3000;
+
 app.set('port', port);
 app.listen(port);
 console.log('Running on http://' + hostname + ':' + port);
